8c189eefd15c38afbcbd22b2b1d8335316c09d4e
Husseiny <husseiny.mohammed@gmail.com>
2020-08-25 16:43:55 +0200

    modified controllers to pass query to db

--- src/controllers/coffeeMachineController.js
+++ src/controllers/coffeeMachineController.js
@@ -3,6 +3,6 @@ const HTTPStatus = require('http-status');
 const CoffeeMachine = mongoose.model('CoffeeMachine');
 
 exports.getMachines = async (req, res) => {
-  const coffeeMachines = await CoffeeMachine.find().sort({_id :'asc'});
+  const coffeeMachines = await CoffeeMachine.find(req.query).sort({ _id: 'asc' });
   res.status(HTTPStatus.OK).json(coffeeMachines);
 };
--- src/controllers/coffeePodController.js
+++ src/controllers/coffeePodController.js
@@ -3,6 +3,6 @@ const HTTPStatus = require('http-status');
 const CoffeePod = mongoose.model('CoffeePod');
 
 exports.getPods = async (req, res) => {
-  const coffeePods = await CoffeePod.find().sort({_id :'asc'});
+  const coffeePods = await CoffeePod.find(req.query).sort({ _id: 'asc' });
   res.status(HTTPStatus.OK).json(coffeePods);
 };
