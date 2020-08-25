32f3759095330f0ef44eef4f872cbb5ea485ce2e
Husseiny <husseiny.mohammed@gmail.com>
2020-08-25 13:26:31 +0200

    completed models schema

--- models/CoffeeMachine.js
+++ models/CoffeeMachine.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+
 const machineSchema = new mongoose.Schema({
-  sku: String
+  sku: { type: String, unique: [true, 'sku must be unique'], required: [true, 'sku is required'] },
+  productType: {
+    type: String,
+    enum: ['COFFEE_MACHINE_LARGE', 'COFFEE_MACHINE_SMALL', 'ESPRESSO_MACHINE'],
+  },
+  waterLineCompatible: { type: Boolean, required: [true, 'waterLineCompatible is required'] },
+  model: { type: String, enum: ['base', 'premium', 'deluxe'] },
 });
 
 module.exports = mongoose.model('CoffeeMachine', machineSchema);
--- models/CoffeePod.js
+++ models/CoffeePod.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+
 const PodSchema = new mongoose.Schema({
-  sku: String,
+  sku: { type: String, unique: [true, 'sku must be unique'], required: [true, 'sku is required'] },
+  productType: {
+    type: String,
+    enum: ['COFFEE_POD_LARGE', 'COFFEE_POD_SMALL', 'ESPRESSO_POD'],
+  },
+  coffeeFlavor: {
+    type: String,
+    enum: [
+      'COFFEE_FLAVOR_VANILLA',
+      'COFFEE_FLAVOR_CARAMEL',
+      'COFFEE_FLAVOR_PSL',
+      'COFFEE_FLAVOR_MOCHA',
+      'COFFEE_FLAVOR_HAZELNUT',
+    ],
+  },
+  packSize: {
+    type: Number,
+    enum: [1, 3, 5, 7],
+  },
 });
 
 module.exports = mongoose.model('CoffeePod', PodSchema);
